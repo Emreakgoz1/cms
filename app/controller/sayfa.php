db06c23f7941631595168349781e8a20e7cacd32
Emreakgoz1 <baranemreakgoz@gmail.com>
2023-04-10 02:58:48 +0300

    hakkimda echo siscard

--- app/controller/sayfa.php
+++ app/controller/sayfa.php
@@ -1,14 +1,11 @@
 <?php
-
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-/*$page_url = route(1);
+$page_url = route(1);
 if (!$page_url) {
     header('Location:' . site_url('404'));
 }
-echo $page_url;
 $row = $db->from('pages')
     ->where('page_url', $page_url)
     ->first();
@@ -23,6 +20,6 @@ $seo = json_decode($row['page_seo'], true);
 $meta = [
     'title' => $seo['title'] ? $seo['title'] : $row['page_title'],
     'description' => $seo['description'] ? $seo['description'] : cut_text($row['page_content'], 210)
-];*/
+];
 
-require view('hakkimda');
+require view('page');
